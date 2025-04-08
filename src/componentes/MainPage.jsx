81a7ca1f1c8ab6a61af5891594a61690425c2af9
Anders0nlima <andersoncon85@gmail.com>
2025-04-08 16:18:00 -0300

    teste1

---
+++ src/componentes/MainPage.jsx
@@ -0,0 +1,594 @@
+import React from "react";
+
+import imagem1 from "../assets/imagem1.jpg";
+import imagem2 from "../assets/imagem2.jpg";
+import imagem3 from "../assets/imagem3.jpg";
+import imagem4 from "../assets/imagem4.png";
+import imagem5 from "../assets/imagem5.jpg";
+import imagem6 from "../assets/imagem6.jpg";
+import imagem7 from "../assets/imagem7.jpg";
+import imagem8 from "../assets/imagem8.jpg";
+import imagem9 from "../assets/imagem9.jpg";
+/*
+import image11 from "./image-1.png";
+import image2 from "./image-2.png";
+import image3 from "./image-3.png";
+import image1 from "./image.png";
+import image from "./image.svg";
+import images1 from "./images-1.png";
+import line1 from "./line-1.svg";
+import line2 from "./line-2.svg";
+import line3 from "./line-3.svg";
+import line10 from "./line-10.svg";
+import line112 from "./line-11-2.svg";
+import line113 from "./line-11-3.svg";
+import line114 from "./line-11-4.svg";
+import line115 from "./line-11-5.svg";
+import line116 from "./line-11-6.svg";
+import line117 from "./line-11-7.svg";
+import line118 from "./line-11-8.svg";
+import line11 from "./line-11.svg";
+import line122 from "./line-12-2.svg";
+import line123 from "./line-12-3.svg";
+import line124 from "./line-12-4.svg";
+import line125 from "./line-12-5.svg";
+import line126 from "./line-12-6.svg";
+import line127 from "./line-12-7.svg";
+import line128 from "./line-12-8.svg";
+import line129 from "./line-12-9.svg";
+import line12 from "./line-12.svg";
+import line13 from "./line-13.svg";
+import line14 from "./line-14.svg";
+import line15 from "./line-15.svg";
+import line16 from "./line-16.svg";
+import line17 from "./line-17.svg";
+import line18 from "./line-18.svg";
+import line19 from "./line-19.svg";
+*/
+
+/*
+import maskGroup from "./mask-group.png";
+import phantomThread20172188685 from "./phantom-thread-2017-218-8685.png";
+*/
+/*
+import "./style.css";
+*/
+/*
+import thereWillBeBlood200715059 from "../assets/there-will-be-blood-2007-15059.jpg";
+import vector2 from "./vector-2.svg";
+import vector3 from "./vector-3.svg";
+import vector from "./vector.svg";
+*/
+
+export const PgProduo = () => {
+  return (
+    <div className="pg-produo">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+
+          <div className="rectangle-2" />
+
+          <div className="text-wrapper">Mapa Espaciais</div>
+
+          <div className="simula-es-t-cnicas">
+            Simulações&nbsp;&nbsp;Técnicas
+          </div>
+        </div>
+
+        <div className="overlap-group">
+          <div className="text-wrapper">Técnicas Criativas</div>
+        </div>
+
+        <div className="div-wrapper">
+          <div className="text-wrapper-2">Estudos Dinâmicos</div>
+        </div>
+
+        <div className="overlap-2">
+          <div className="overlap-3">
+            <div className="rectangle-3" />
+
+            <div className="text-wrapper-3">APLICAÇÕES</div>
+
+            <div className="text-wrapper-4">PROFISSIONAIS EXPERIENTES</div>
+
+            <p className="PROFISSIONAIS">
+              <span className="span">PROFISSIONAIS </span>
+
+              <span className="span">RECOMENDADOS</span>
+            </p>
+
+            <div className="text-wrapper-5">ESPECIALISTAS DE PRODUÇÃO</div>
+
+            <p className="p">SETORES DE ATUAÇÃO DA SELO</p>
+
+            <div className="TRANSFORME-SUA">
+              TRANSFORME SUA MANEIRA DE&nbsp;&nbsp;PRODUZIR
+            </div>
+
+            <p className="text-wrapper-6">
+              Conte com os melhores proposicionais da área, e com experiência,
+              possibilitando maior rendimento e otimização da produção.
+            </p>
+
+            <img className="element" alt="Element" src={imagem1} />
+
+            
+
+            <p className="text-wrapper-7">
+              A pré-produção cinematográfica é a fase de planejamento do filme,
+              incluindo roteiro, orçamento, cronograma, seleção de elenco e
+              equipe.
+            </p>
+
+            <p className="text-wrapper-8">
+              A pré-produção cinematográfica é a fase de planejamento do filme,
+              incluindo roteiro, orçamento, cronograma, seleção de elenco e
+              equipe.
+            </p>
+
+            <div className="text-wrapper-9">MEU INTERESSE</div>
+
+            <div className="text-wrapper-10">Aplicações da selo</div>
+
+            <div className="text-wrapper-11">Pré produção</div>
+
+            <p className="text-wrapper-12">
+              A pré-produção cinematográfica é a fase de planejamento do filme,
+              incluindo roteiro, orçamento, cronograma, seleção de elenco e
+              equipe, escolha de locações e definição de aspectos visuais. Essa
+              etapa é essencial para garantir que as filmagens ocorram de forma
+              organizada e eficiente.
+            </p>
+
+            <div className="rectangle-4" />
+
+            <div className="rectangle-5" />
+
+            <img className="abstract" alt="Abstract" src={imagem2} />
+
+            <div className="text-wrapper-13">Aplicações da selo</div>
+
+            <div className="frame">
+              <div className="ellipse" />
+
+              <div className="ellipse" />
+            </div>
+
+            <div className="text-wrapper-14">Aplicações da selo</div>
+
+            <div className="text-wrapper-15">Pré produção</div>
+
+            <p className="text-wrapper-16">
+              A pré-produção cinematográfica é a fase de planejamento do filme,
+              incluindo roteiro, orçamento, cronograma, seleção de elenco e
+              equipe, escolha de locações e definição de aspectos visuais. Essa
+              etapa é essencial para garantir que as filmagens ocorram de forma
+              organizada e eficiente.
+            </p>
+
+            <div className="rectangle-6" />
+
+            <img className="abstract-2" alt="Abstract" src={imagem3} />
+
+            <div className="text-wrapper-17">Aplicações da selo</div>
+
+            <div className="frame-2">
+              <div className="ellipse" />
+
+              <div className="ellipse" />
+            </div>
+
+           
+
+            <div className="rectangle-7" />
+
+            <div className="rectangle-7" />
+
+            <div className="rectangle-8" />
+
+            <div className="text-wrapper-18">Experiência</div>
+
+            <div className="rectangle-9" />
+
+            <div className="group">
+              <div className="overlap-4">
+                <div className="text-wrapper-19">MEU INTERESSE</div>
+
+                <div className="rectangle-10" />
+
+                <div className="rectangle-11" />
+
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="overlap-wrapper">
+              <div className="overlap-group-2">
+               
+              </div>
+            </div>
+
+            <div className="rectangle-12" />
+          </div>
+
+          <div className="text-wrapper-20">COLABORADORES</div>
+
+        
+
+          <div className="overlap-5">
+            <div className="text-wrapper-21">Otimização</div>
+          </div>
+
+          <div className="overlap-6">
+            <div className="text-wrapper-22">Organização</div>
+          </div>
+
+          <div className="group-2">
+            <div className="overlap-group-3">
+             
+            </div>
+          </div>
+
+          <div className="group-3">
+            <div className="overlap-7">
+              
+            </div>
+          </div>
+
+          <div className="group-4">
+            <div className="overlap-group-3">
+             
+            </div>
+          </div>
+        </div>
+
+        <div className="overlap-8">
+          <div className="overlap-9">
+            <div className="text-wrapper-23">Experiência</div>
+          </div>
+
+          <div className="overlap-10">
+            <div className="text-wrapper-24">Otimização</div>
+          </div>
+
+          <div className="overlap-11">
+            <div className="text-wrapper-25">Organização</div>
+          </div>
+
+          <div className="text-wrapper-26">INSIGHT</div>
+
+          <div className="text-wrapper-27">DESCOBRA A SELO</div>
+
+          <p className="text-wrapper-28">
+            Conte com os melhores proposicionais da área, e com experiência,
+            possibilitando maior rendimento e otimização da produção.
+          </p>
+
+      
+
+          <div className="overlap-12">
+            <div className="group-5">
+              <div className="overlap-13">
+                <div className="text-wrapper-29">
+                  Produções cinematográficos e comerciais
+                </div>
+
+                <p className="text-wrapper-30">
+                  A pré-produção cinematográfica é a fase de planejamento do
+                  filme, incluindo roteiro, orçamento, cronograma, seleção de
+                  elenco e equipe, escolha de
+                </p>
+
+                <div className="rectangle-13" />
+
+                <div className="text-wrapper-31">Aplicações da selo</div>
+
+                <div className="frame-3">
+                  <div className="ellipse-2" />
+
+                  <div className="ellipse-2" />
+                </div>
+              </div>
+            </div>
+
+            <img
+              className="there-will-be-blood"
+              alt="There will be blood"
+              src={imagem5}
+            />
+          </div>
+
+          <div className="group-6">
+            <div className="overlap-14">
+              <div className="group-7">
+                <div className="overlap-group-4">
+                  <div className="text-wrapper-32">
+                    Produções cinematográficos e comerciais
+                  </div>
+
+                  <p className="text-wrapper-33">
+                    A pré-produção cinematográfica é a fase de planejamento do
+                    filme, incluindo roteiro, orçamento, cronograma, seleção de
+                    elenco e equipe, escolha de
+                  </p>
+
+                  <div className="rectangle-14" />
+
+                  <div className="text-wrapper-34">Aplicações da selo</div>
+
+                  <div className="frame-4">
+                    <div className="ellipse-3" />
+
+                    <div className="ellipse-4" />
+                  </div>
+                </div>
+              </div>
+
+              <img className="mask-group" alt="Mask group" src={imagem3} />
+            </div>
+          </div>
+
+          <div className="group-8">
+            <div className="overlap-15">
+              <div className="group-9">
+                <div className="overlap-group-4">
+                  <div className="text-wrapper-32">
+                    Produções cinematográficos e comerciais
+                  </div>
+
+                  <p className="text-wrapper-33">
+                    A pré-produção cinematográfica é a fase de planejamento do
+                    filme, incluindo roteiro, orçamento, cronograma, seleção de
+                    elenco e equipe, escolha de
+                  </p>
+
+                  <div className="rectangle-14" />
+
+                  <div className="text-wrapper-34">Aplicações da selo</div>
+
+                  <div className="frame-4">
+                    <div className="ellipse-3" />
+
+                    <div className="ellipse-4" />
+                  </div>
+                </div>
+              </div>
+
+              <img className="mask-group-2" alt="Mask group" src={imagem1} />
+            </div>
+          </div>
+        </div>
+
+        <div className="overlap-16">
+          <div className="group-10">
+            <img className="vector" alt="Vector" src={imagem2} />
+
+            <img className="group-11" alt="Group" src={imagem6} />
+          </div>
+
+          <div className="overlap-17">
+            <div className="text-wrapper-35">IDIOMA</div>
+
+            <div className="rectangle-15" />
+
+            <img className="images" alt="Images" src={imagem1} />
+          </div>
+
+          <img
+            className="icon-magnifying"
+            alt="Icon magnifying"
+            src={imagem7}
+          />
+        </div>
+
+        <div className="overlap-18">
+          <div className="overlap-19">
+            <img
+              className="phantom-thread"
+              alt="Phantom thread"
+              src={imagem6}
+            />
+
+            <div className="rectangle-16" />
+
+            <div className="rectangle-17" />
+
+            <div className="group-12">
+              <img className="vector-2" alt="Vector" src={imagem7} />
+
+              <img className="group-13" alt="Group" src={imagem5} />
+            </div>
+
+            <div className="navbar-wrapper">
+              <div className="navbar">
+                <div className="text-wrapper-36">Visão geral</div>
+
+                <div className="text-wrapper-36">Aplicações</div>
+
+                <div className="text-wrapper-36">Colaboradores</div>
+
+                <div className="text-wrapper-36">Métodos</div>
+
+                <div className="text-wrapper-36">Insight</div>
+              </div>
+            </div>
+
+            <div className="text-wrapper-37">Assista</div>
+
+            <p className="escolha-a-melhor-op">
+              Escolha a melhor opção para sua produção cinematográfica, <br />
+              retire suas ideias do papel e veja como podemos ajudar você <br />
+              a produzir mais.
+            </p>
+
+            <img className="line-16" alt="Line" src={imagem4} />
+
+            <div className="rectangle-18" />
+
+            <img
+              className="icon-play-circle"
+              alt="Icon play circle"
+              src={imagem9}
+            />
+          </div>
+
+          <div className="text-wrapper-38">Produções cinematográficas</div>
+
+          <div className="text-wrapper-39">Negócios</div>
+        </div>
+
+        <div className="text-wrapper-40">Visão geral</div>
+
+        <div className="overlap-20">
+          <img className="image" alt="Image" src={imagem9} />
+
+          <div className="rectangle-19" />
+
+          <div className="group-14">
+            <div className="overlap-21">
+              <div className="text-wrapper-41">Assista</div>
+
+              <div className="rectangle-20" />
+
+              <img
+                className="icon-play-circle-2"
+                alt="Icon play circle"
+                src={imagem8}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="text-wrapper-42">Métodos</div>
+
+        <div className="text-wrapper-43">PRODUZA MAIS COM ORGANIZAÇÃO</div>
+
+        <p className="text-wrapper-44">
+          A pré-produção cinematográfica é a fase de planejamento do filme,
+          incluindo roteiro, orçamento, cronograma, seleção de elenco e equipe,
+          escolha de locações e definição de aspectos visuais. Essa etapa é
+          essencial para garantir que as filmagens ocorram de forma organizada e
+          eficiente.
+        </p>
+
+        <p className="text-wrapper-45">
+          A pré-produção cinematográfica é a fase de planejamento do filme,
+          incluindo roteiro, orçamento, cronograma.
+        </p>
+
+        
+
+        <div className="overlap-22">
+          <div className="rectangle-21" />
+
+          <div className="text-wrapper-23">Experiência</div>
+
+          <div className="text-wrapper-23">Experiência</div>
+        </div>
+
+        <div className="overlap-23">
+          <div className="text-wrapper-46">Resultado Final</div>
+
+          
+        </div>
+
+        <div className="overlap-24">
+          <div className="text-wrapper-47">MEU INTERESSE</div>
+
+          <div className="text-wrapper-48">Estudo Dinâmico</div>
+
+          <div className="rectangle-22" />
+
+          <p className="text-wrapper-49">Desenho de Blocagem de fotografia</p>
+
+          <img className="image-2" alt="Image" src={imagem2} />
+
+          <p className="text-wrapper-50">
+            A pré-produção cinematográfica é a fase de planejamento do filme,
+            incluindo roteiro, orçamento, cronograma, seleção de elenco e
+            equipe, escolha de locações e definição de aspectos visuais. Essa
+            etapa é essencial para garantir que as filmagens ocorram de forma
+            organizada e eficiente.
+          </p>
+
+          <div className="group-15">
+            <img className="vector-3" alt="Vector" src={imagem5} />
+
+            <img className="group-16" alt="Group" src={imagem4} />
+          </div>
+        </div>
+
+        <div className="overlap-25">
+          <div className="text-wrapper-51">Selo produções cinematográficas</div>
+
+          <div className="rectangle-23" />
+
+          <div className="text-wrapper-52">Resultado Final</div>
+
+          <div className="frame-5">
+            <div className="ellipse-5" />
+
+            <div className="ellipse-5" />
+          </div>
+        </div>
+
+        <div className="TRANSFORME-SUA-2">
+          TRANSFORME SUA MANEIRA DE&nbsp;&nbsp;PRODUZIR
+        </div>
+
+        <p className="text-wrapper-53">
+          A pré-produção cinematográfica é a fase de planejamento do filme,
+          incluindo roteiro, orçamento, cronograma, seleção de elenco e equipe,
+          cronograma, seleção de elenco e equipe, escolha de locações e
+          definição de aspectos visuais. Essa etapa
+        </p>
+
+        <div className="image-wrapper">
+          <img className="image-3" alt="Image" src={imagem3} />
+        </div>
+
+        <div className="text-wrapper-54">PROFISSIONAIS EXPERIENTES</div>
+
+        <div className="text-wrapper-55">PROFISSIONAIS RECOMENDADOS</div>
+
+        <div className="text-wrapper-56">ESPECIALISTAS DE PRODUÇÃO</div>
+
+        <p className="text-wrapper-57">
+          Conte com os melhores proposicionais da área, e com experiência,
+          possibilitando maior rendimento e otimização da produção.
+        </p>
+
+        
+
+        <div className="group-17">
+          <div className="overlap-26">
+            
+          </div>
+        </div>
+
+        <div className="group-18">
+          <div className="overlap-7">
+            
+          </div>
+        </div>
+
+        <div className="group-19">
+          <div className="overlap-7">
+           
+          </div>
+        </div>
+
+        <div className="overlap-27">
+          <div className="rectangle-21" />
+
+          <div className="text-wrapper-58">Destaques</div>
+        </div>
+      </div>
+    </div>
+  );
+};
